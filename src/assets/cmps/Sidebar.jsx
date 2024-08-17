87a83099cd31d8ea8e0f87cef886beec16723e0a
Roy-bitapp <roy@bitapp.co.il>
2024-08-17 23:55:34 +0300

    some sidebar css fixes

--- src/assets/cmps/Sidebar.jsx
+++ src/assets/cmps/Sidebar.jsx
@@ -1,60 +1,60 @@
 import React, { useState } from 'react';
-import compose  from "/src/assets/imgs/sidebar/compose.png";
-import draft  from "/src/assets/imgs/sidebar/draft.png";
-import inbox  from "/src/assets/imgs/sidebar/inbox.png";
-import sent  from "/src/assets/imgs/sidebar/sent.png";
-import sentExpanded  from "/src/assets/imgs/sidebar/sentExpanded.png";
-import star  from "/src/assets/imgs/sidebar/star.png";
-import trash  from "/src/assets/imgs/sidebar/trash.png";
-import trashExpanded  from "/src/assets/imgs/sidebar/trashExpanded.png";
+import compose from "/src/assets/imgs/sidebar/compose.png";
+import draft from "/src/assets/imgs/sidebar/draft.png";
+import inbox from "/src/assets/imgs/sidebar/inbox.png";
+import sent from "/src/assets/imgs/sidebar/sent.png";
+import sentExpanded from "/src/assets/imgs/sidebar/sentExpanded.png";
+import star from "/src/assets/imgs/sidebar/star.png";
+import trash from "/src/assets/imgs/sidebar/trash.png";
+import trashExpanded from "/src/assets/imgs/sidebar/trashExpanded.png";
 
 
 
 
-export function Sidebar(){
+export function Sidebar() {
 
   const [isExpanded, setIsExpanded] = useState(false);
 
-  function toggleSidebar(){
+  function toggleSidebar() {
     setIsExpanded(isExpanded => !isExpanded);
   };
 
-  return(    
-    <div 
-    className={`sidebar ${isExpanded ? 'expanded' : ''}`} 
-    onMouseEnter={toggleSidebar} 
-    onMouseLeave={toggleSidebar}>
+  return (
+    <div
+      className={`sidebar ${isExpanded ? 'expanded' : ''}`}
+      onMouseEnter={toggleSidebar}
+      onMouseLeave={toggleSidebar}>
 
-    <button 
-      className={`compose-button ${isExpanded ? 'expanded' : ''}`} >
+      <button
+        className={`compose-button ${isExpanded ? 'expanded' : ''}`} >
         <img className='compose-icon' src={compose} alt="" />
         {isExpanded && <div className="compose-button-text">Compose</div>}
-    </button>
-      
+      </button>
+
       <div className="sidebar-item">
-      <img className="menu-icon" src={inbox} alt="" />
-      {isExpanded && <div className="sidebar-item-text">Inbox</div>}
+        <img className="menu-icon" src={inbox} alt="" />
+        {isExpanded && <div className="sidebar-item-text">Inbox</div>}
       </div>
 
       <div className="sidebar-item">
-      <img className="menu-icon" src={star} alt="" />
-      {isExpanded && <div className="sidebar-item-text">Starred</div>}
+        <img className="menu-icon" src={star} alt="" />
+        {isExpanded && <div className="sidebar-item-text">Starred</div>}
 
       </div>
       <div className="sidebar-item">
-      <img className="menu-icon" src={isExpanded ? sentExpanded : sent} alt="" />
-      {isExpanded && <div className="sidebar-item-text">Sent</div>}
+        <img className="menu-icon" src={isExpanded ? sentExpanded : sent} alt="" />
+        {isExpanded && <div className="sidebar-item-text">Sent</div>}
 
       </div>
       <div className="sidebar-item">
-      <img className="menu-icon" src={draft} alt="" />
-      {isExpanded && <div className="sidebar-item-text">Drafts</div>}
+        <img className="menu-icon" src={draft} alt="" />
+        {isExpanded && <div className="sidebar-item-text">Drafts</div>}
 
       </div>
       <div className="sidebar-item">
-      <img className="menu-icon" src={isExpanded ? trashExpanded : trash} alt="" />
-      {isExpanded && <div className="sidebar-item-text">Trash</div>}
+        <img className="menu-icon" src={isExpanded ? trashExpanded : trash} alt="" />
+        {isExpanded && <div className="sidebar-item-text">Trash</div>}
       </div>
-  </div>
+    </div>
   )
 }
